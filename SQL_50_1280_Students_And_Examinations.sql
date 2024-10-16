c9b64bf5960d7177f9ed1756599bbce8bb70f060
Saurabh <sygirme13@gmail.com>
2024-10-16 11:18:31 +0530

    SQL_50 Day 8 using Count function and joins

---
+++ SQL_50_1280_Students_And_Examinations.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+
+SELECT stu.student_id, stu.student_name, sub.subject_name, 
+        COUNT(exam.subject_name) AS attended_exams
+
+FROM students AS stu
+CROSS JOIN subjects AS sub
+LEFT JOIN examinations AS exam
+ON
+    stu.student_id = exam.student_id AND
+    exam.subject_name = sub.subject_name
+
+GROUP BY
+    stu.student_id, stu.student_name, sub.subject_name
+ORDER BY
+    stu.student_id, sub.subject_name;
