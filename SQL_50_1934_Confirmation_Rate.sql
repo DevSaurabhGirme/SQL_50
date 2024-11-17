5f44ab3553ecaf11b5ab2ab5bdaf87220ac32727
Saurabh <sygirme13@gmail.com>
2024-11-17 10:37:19 +0530

    SQL_50 Day 10 using left join and AVG

---
+++ SQL_50_1934_Confirmation_Rate.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+SELECT s.user_id,
+    ROUND(AVG(if(c.action = "confirmed", 1, 0)), 2) as confirmation_rate
+FROM Signups AS s
+LEFT JOIN Confirmations as c 
+    ON s.user_id = c.user_id 
+GROUP BY user_id;
